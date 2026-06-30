b502b421f47c94fe0c4bbec4228f45bf06639e64
Talha <talhahdev2025@gmail.com>
2026-07-01 04:37:33 +0500

    practice GridView with all its variations/namedConstructors(custom,build,count,extent)

--- grid_view/lib/main.dart
+++ grid_view/lib/main.dart
@@ -1,5 +1,20 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+final Random random = Random();
+final List<Widget> gridViewItemList = List.generate(100, (index) {
+  return MyGridViewItem(
+    text: index.toString(),
+    color: Color.fromRGBO(
+      random.nextInt(255),
+      random.nextInt(255),
+      random.nextInt(255),
+      1,
+    ),
+  );
+});
 void main() {
   runApp(const MyApp());
 }
@@ -11,9 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -21,24 +34,12 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,21 +49,141 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Expanded(child: GridContainer(child: SimpleGridView())),
+            Expanded(child: GridContainer(child: GridViewCount())),
+            Expanded(child: GridContainer(child: GridViewBuilder())),
+            Expanded(child: GridContainer(child: GridViewCustom())),
+            Expanded(child: GridContainer(child: GridViewExtent())),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class MyGridViewItem extends StatelessWidget {
+  final String text;
+  final Color? color;
+  MyGridViewItem({required this.text, required this.color});
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      // margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: color,
       ),
+      child: Center(
+        child: Text('$text', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
+
+class GridContainer extends StatelessWidget {
+  Color? color;
+  Widget child;
+
+  GridContainer({required this.child, this.color});
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      clipBehavior: .hardEdge,
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: child,
+    );
+  }
+}
+
+//grid variations
+class SimpleGridView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GridView(
+      // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      //   crossAxisCount: 5,
+      //   crossAxisSpacing: 10,
+      //   mainAxisSpacing: 10,
+      // ),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 50,
+        mainAxisExtent: 50,
+        mainAxisSpacing: 5,
+        crossAxisSpacing: 5,
+      ),
+      children: gridViewItemList,
+    );
+  }
+}
+
+class GridViewCount extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 5,
+      mainAxisSpacing: 5,
+      crossAxisSpacing: 5,
+      physics: BouncingScrollPhysics(),
+      children: gridViewItemList,
+    );
+  }
+}
+
+class GridViewBuilder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GridView.builder(
+      itemCount: 100,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 5,
+        mainAxisSpacing: 5,
+      ),
+      itemBuilder: (context, index) {
+        return MyGridViewItem(text: '$index', color: Colors.redAccent);
+      },
+    );
+  }
+}
+
+class GridViewCustom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GridView.custom(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 30,
+        mainAxisExtent: 30,
+        crossAxisSpacing: 5,
+        mainAxisSpacing: 5,
+      ),
+      // childrenDelegate: SliverChildListDelegate(gridViewItemList),
+      childrenDelegate: SliverChildBuilderDelegate((context, index) {
+        return MyGridViewItem(text: '$index', color: Colors.orangeAccent);
+      }, childCount: 100),
+    );
+  }
+}
+
+class GridViewExtent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GridView.extent(
+      maxCrossAxisExtent: 50,
+      mainAxisSpacing: 5,
+      crossAxisSpacing: 5,
+      scrollDirection: .horizontal,
+      children: gridViewItemList,
     );
   }
 }
