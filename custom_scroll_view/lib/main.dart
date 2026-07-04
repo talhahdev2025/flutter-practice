328f751f3f93dacecbbd4082e3fdee0b6f6b1aee
Talha <talhahdev2025@gmail.com>
2026-07-04 20:45:52 +0500

    CustomScrollView commonly used properties

--- custom_scroll_view/lib/main.dart
+++ custom_scroll_view/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -36,12 +37,93 @@ class _MyHomePageState extends State<MyHomePage> {
       body: CustomScrollView(
         slivers: [
           SliverList.builder(
+            itemCount: 30,
             itemBuilder: (context, index) {
               return ListTile(
                 title: Text('this is title'),
-                leading: CircleAvatar(backgroundColor: Colors.orange[200],),
+                leading: CircleAvatar(backgroundColor: Colors.orange[200]),
               );
             },
+          ),
+          SliverGrid.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+              ),
+            itemCount: 10,
+
+            itemBuilder: (context, index) =>
+                Container(color: Colors.pinkAccent, height: 50),
+            /*
+            [
+              Container(color: Colors.pinkAccent, height: 50),
+              Container(color: Colors.pinkAccent, height: 50),
+              Container(color: Colors.pinkAccent, height: 50),
+              Container(color: Colors.pinkAccent, height: 50),
+              Container(color: Colors.pinkAccent, height: 50),
+              Container(color: Colors.pinkAccent, height: 50),
+              Container(color: Colors.pinkAccent, height: 50),
+              Container(color: Colors.pinkAccent, height: 50),
+              Container(color: Colors.pinkAccent, height: 50),
+              Container(color: Colors.pinkAccent, height: 50),
+            ],
+            */
+          ),
+          SliverToBoxAdapter(
+            child: Center(
+              child: Container(
+                // color: Colors.amber,
+                width: 200,
+                height: 300,
+                child: Stack(
+                  alignment: .center,
+                  children: [
+                    Transform.rotate(
+                      angle: pi / 50,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.pinkAccent,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+
+                        height: 100,
+                      ),
+                    ),
+                    Transform.rotate(
+                      angle: -pi / 50,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        // width: 100,
+                        height: 100,
+                      ),
+                    ),
+                    Transform.rotate(
+                      angle: pi / 20,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.cyanAccent,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        // width: 100,
+                        height: 100,
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.teal,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      // width: 100,
+                      height: 100,
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
